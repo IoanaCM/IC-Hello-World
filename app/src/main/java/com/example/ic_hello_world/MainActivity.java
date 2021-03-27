0a1d538d7a38af09f2e21183931840921c6a4838
Ioana <ioanamihailecu02@yahoo.com>
2021-03-27 18:02:47 +0000

    fixed log in

--- app/src/main/java/com/example/ic_hello_world/MainActivity.java
+++ app/src/main/java/com/example/ic_hello_world/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.ic_hello_world;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mLogOut;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mLogOut = (Button) findViewById(R.id.log_out);
+        mLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                startActivity(intent);
+                finish();
+                return;
+                            }
+        });
     }
+
+
 }
